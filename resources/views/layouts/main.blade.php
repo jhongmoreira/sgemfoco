704a63b21ee50a26ae9052ae44dea6332aa28c06
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-02-03 19:17:34 -0300

    Rotas

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
         <header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php">SANGO+</a>
+                    <a class="navbar-brand" href="{{ route('home') }}">SANGO+</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -25,7 +25,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home</a>
+                                <a class="nav-link" href="{{ route('home') }}">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="index.php?pg=8">Feed</a>
@@ -50,7 +50,7 @@
       
       <footer class="py-3 my-1">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+          <li class="nav-item"><a href="{{ route('home') }}" class="nav-link px-2 text-body-secondary">Home</a></li>
           <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Afilie-se</a></li>
           <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">SAC</a></li>
         </ul>
