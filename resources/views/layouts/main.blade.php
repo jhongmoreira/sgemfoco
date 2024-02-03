4a92904cc06845c56c7cc5f59cc5591c91f10764
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-02-03 19:09:17 -0300

    Página inicial

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,51 @@
         <link rel="stylesheet" href="cdn/fa/css/solid.min.css">
     </head>
     <body>
-        @yield('content')
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">SANGO+</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?pg=8">Feed</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Currículos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?pg=3">WebTV</a>
+                            </li>
+                        </ul>
+                        <!-- <form class="d-flex" role="search">
+                        <input class="form-control me-2 bg-dark text-white" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-warning" type="submit">Pesquisas</button>
+                    </form> -->
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+      @yield('content')
+      
+      <footer class="py-3 my-1">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Afilie-se</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">SAC</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">© 2023 SG+</p>
+      </footer>
+      <!-- JavaScript -->
+      <script src="https://unpkg.com/@popperjs/core@2"></script>
+      <script src="cdn/bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,192 @@
 @extends('layouts.main')
 @section('title','Página Principal - SG em Foco')
 @section('content')
-    <h1>Teste de página inicial</h1>
+<div id="destaques" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#destaques" data-bs-slide-to="0" class="active" aria-current="true"
+            aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#destaques" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <!-- <button type="button" data-bs-target="#destaques" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
+    </div>
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="banner-1"></div>
+            <div class="container texto-container">
+                <div class="carousel-caption text-start">
+                    <h1>Supermercado Central</h1>
+                    <p>Sua compra com o melhor preço da cidade.</p>
+                    <p><a class="btn btn-sm btn-primary" href="#">Acessar Feed</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class="banner-2"></div>
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Farmácia São Sebastião</h1>
+                    <p>Descontos imperdíveis em medicamentos genéricos</p>
+                    <p><a class="btn btn-sm btn-primary" href="#">Acessar Feed</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#destaques" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#destaques" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+
+<section class="container-fluid mt-4">
+
+    <!-- <div class="row">
+        <div class="col-md-12 text-center">
+          <h3 class="p-0 m-0">Pesquise por uma empresa</h3>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mb-5 mt-3">
+        <div class="col-md-8">
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Digite a empresa ou categoria" aria-label="Search">
+            <button class="btn btn-sm btn-primary" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </div> -->
+
+    <div class="container">
+
+        <div class="row mb-2">
+            <div class="col-md-12 titulos-home-border-left">
+                <h3 class="border-titulo">
+                    <span class="titulos-home">
+                        Classificados
+                    </span>
+                </h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <div class="card" style="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <span>Imóvel</span>
+                        </h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            <span>Aluga-se imóvel no residêncial garcia no valor de R$ 1.500,00/mês</span>
+                        </h6>
+                        <a href="index.php"
+                            class="card-link">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row mb-2">
+            <div class="col-md-12 titulos-home-border-left">
+                <h3 class="border-titulo">
+                    <span class="titulos-home">
+                        Empresas
+                    </span>
+                </h3>
+            </div>
+        </div>
+
+        <div class="friend-list">
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="friend-card card">
+                        <img src="img/conteudo/background_profile.jpg" alt="profile-cover" class="img-responsive cover">
+                        <div class="card-info">
+                            <img src="img/conteudo/logo_eficaz.png" alt="user"
+                                class="profile-photo-lg">
+                            <div class="friend-info">
+                                <p class="float-end text-green">São Gotardo</p>
+                                <h5><a href="index.php"
+                                        class="profile-link">
+                                       Efiz Desentupidora
+                                    </a></h5>
+                                <p class="categoria-empresa">
+                                    Serviços
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 mt-3 mb-5 d-flex justify-content-left">
+                <a href="index.php?pg=5" class="btn btn-sm btn-primary">
+                    Ver Empresas
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="p-5 mb-5 bg-dark text-white rounded">
+                    <h1>Tenha sua presença aqui</h1>
+                    <p>Cadastre sua empresa, vagas de emprego e acesse currículos de São Gotardo e região.</p>
+                    <button class="btn btn-secondary">Cadastrar-se</button>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row mb-2">
+            <div class="col-md-12 titulos-home-border-left">
+                <h3 class="border-titulo">
+                    <span class="titulos-home">
+                        Vagas de Emprego
+                    </span>
+                </h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <div class="card" style="height: 7.5em;">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Designer Gráfico
+                        </h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            Atual Agência 
+                        </h6>
+                        <p class="card-text">
+                            <span class="categoria badge bg-sucess ?>">
+                                Disponível
+                            </span>
+                        </p>
+                        <a href="index.php" class="card-link">Ver Vaga</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 mt-3 mb-5 d-flex justify-content-left">
+                <a href="index.php?pg=5" class="btn btn-sm btn-primary">
+                    Ver Todas
+                </a>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+</section>
 @endsection
